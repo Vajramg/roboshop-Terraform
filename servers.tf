f9827dfed75ce8a61abc3520634ee85151078cb9
Vajram Gajengi <vajramg@gmail.com>
2023-07-28 21:52:11 +0530

    servers creations

---
+++ servers.tf
@@ -0,0 +1,98 @@
+resource "aws_instance" "frontend" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_instance" "Mongodb" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Mongodb"
+  }
+}
+
+resource "aws_instance" "Catalogue" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Catalogue"
+  }
+}
+
+resource "aws_instance" "Redis" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Redis"
+  }
+}
+
+resource "aws_instance" "User" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "User"
+  }
+}
+
+resource "aws_instance" "Cart" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "cart"
+  }
+}
+
+resource "aws_instance" "MySQL" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "MySQL"
+  }
+}
+
+resource "aws_instance" "Shipping" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Shipping"
+  }
+}
+
+resource "aws_instance" "RabbitMQ" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "RabbitMQ"
+  }
+}
+
+resource "aws_instance" "Payment" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Payment"
+  }
+}
+
+resource "aws_instance" "Dispatch" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Dispatch"
+  }
+}
